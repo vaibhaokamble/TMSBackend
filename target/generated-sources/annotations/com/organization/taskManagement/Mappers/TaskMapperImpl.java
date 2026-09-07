c4bbe9999af48967a782ab55ef7862a734574c1d
Vaibhao Kamble <vaibhaokamble@gmail.com>
2026-09-07 15:00:41 +0530

    fix the bugs

--- target/generated-sources/annotations/com/organization/taskManagement/Mappers/TaskMapperImpl.java
+++ target/generated-sources/annotations/com/organization/taskManagement/Mappers/TaskMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-09-03T22:23:17+0530",
+    date = "2026-09-07T14:45:05+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.11 (Oracle Corporation)"
 )
 @Component
